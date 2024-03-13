f699517ee4f387fb92a14e0e464303ad54643eb8
Diogo Matheus <diogo.costa@senac.br>
2024-03-13 15:16:21 -0300

    fix: adminlte pushmenu

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <nav className="main-header navbar navbar-expand navbar-white navbar-light">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="#" className="nav-link" data-widget="pushmenu" data-enable-remember="true" role="button"><i className="fas fa-bars"></i></a>
+          <a href="#" className="nav-link" data-widget="pushmenu" data-enable-remember="false" role="button"><i className="fas fa-bars"></i></a>
         </li>
       </ul>
     </nav>
