707586bf0054b93316d0415308613f95d443c9e4
Diogo Matheus <diogo.costa@senac.br>
2024-03-13 15:12:05 -0300

    fix: next configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === "production" ? '/vocational-dashboard' : '',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
