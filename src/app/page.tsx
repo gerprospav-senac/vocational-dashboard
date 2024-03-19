5f471c61f3347d67e76905dbd84f89ce9f6eb899
Diogo Matheus <diogo.costa@senac.br>
2024-03-19 14:18:02 -0300

    feat: educational profile with mocked datatables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef } from "react";
+import { RefObject, useEffect, useRef } from "react";
 
 import ChartPieBuilder from "@/component/visualization/chart/chart-pie.builder"
 import TableBuilder from "@/component/visualization/table/table.builder"
@@ -10,9 +10,31 @@ import 'datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css'
 import 'datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css'
 import 'datatables.net-searchpanes-bs4/css/searchPanes.bootstrap4.min.css'
 
+const _table = (reference : RefObject<HTMLTableElement>) => {
+  let datatableGeneral : any = null;
+  const _build = async () => {
+    const builder = new TableBuilder(reference.current!.id)
+      .setData([{ name: 'Lorem Ipsum', type: 'XPTO', value: 20 }])
+      .setColumns([
+        { title: 'Título', data: 'name' },
+        { title: 'Tipo', data: 'type' },
+        { title: 'Nº de matrículas', data: 'value' }
+      ]);
+
+    datatableGeneral = await builder.build();
+  };
+  _build();
+  return () => {
+    datatableGeneral?.clear();
+    datatableGeneral?.destroy();
+  };
+};
+
 export default function Home() {
   const chartRef = useRef<HTMLTableElement>(null);
-  const tableRef = useRef<HTMLTableElement>(null);
+  const tableGeneralRef = useRef<HTMLTableElement>(null);
+  const tablePSGRef = useRef<HTMLTableElement>(null);
+  const tableCommercialRef = useRef<HTMLTableElement>(null);
 
   useEffect(() => {
     document.title = 'Perfil Educacional | Vocacional';
@@ -32,25 +54,15 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    let datatable : any = null;
-    
-    const _build = async () => {
-      const builder = new TableBuilder(tableRef.current!.id)
-        .setData([{ type: 'XPTO', value: 20, xpto: 0 }])
-        .setColumns([
-          { title: 'Type', data: 'type' },
-          { title: 'Publications', data: 'value' },
-          { title: 'XPTO', data: 'xpto' }
-        ]);
+    return _table(tableGeneralRef);
+  }, []);
 
-      datatable = await builder.build();
-    };
+  useEffect(() => {
+    return _table(tablePSGRef);
+  }, []);
 
-    _build();
-    return () => {
-      datatable?.clear();
-      datatable?.destroy();
-    };
+  useEffect(() => {
+    return _table(tableCommercialRef);
   }, []);
 
   return (
@@ -106,26 +118,64 @@ export default function Home() {
               <h3 className="card-title">Container</h3>
             </div>
             <div className="card-body">
-              <table
-                id="datatable-geographic-distribution"
-                className="table table-striped table-bordered responsive-table datatable-visualization"
-                style={{ width: "100%" }}
-                ref={tableRef}
-                />
-              <br />
               <div id="chart-geographic-distribution" className="amcharts-visualization" ref={chartRef}></div>
             </div>
           </div>
-
+          
           <div className="card">
             <div className="card-header">
-              <h3 className="card-title">Container</h3>
+              <h3 className="card-title">Principais Títulos</h3>
+              <div className="card-tools">
+              <button type="button" className="btn btn-tool" data-card-widget="maximize"><i className="fas fa-expand"></i>
+              </button>
+              </div>
             </div>
             <div className="card-body">
-              Page content
+              <div className="card card-primary card-tabs">
+                <div className="card-header p-0 pt-1">
+                  <ul className="nav nav-tabs" role="tablist">
+                    <li className="nav-item">
+                      <a className="nav-link active" data-toggle="pill" id="highlight-titles-general-tab" href="#highlight-titles-general" aria-controls="highlight-titles-general" aria-selected="true" role="tab">Geral</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" data-toggle="pill" id="highlight-titles-psg-tab" href="#highlight-titles-psg" aria-controls="highlight-titles-psg" aria-selected="false" role="tab">PSG</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" data-toggle="pill" id="highlight-titles-commercial-tab" href="#highlight-titles-commercial" aria-controls="highlight-titles-commercial" aria-selected="false" role="tab">Comercial</a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="card-body">
+                  <div className="tab-content">
+                    <div className="tab-pane fade show active" id="highlight-titles-general" aria-labelledby="highlight-titles-general-tab" role="tabpanel">
+                    <table
+                      id="datatable-highlight-title-general"
+                      className="table table-striped table-bordered responsive-table datatable-visualization"
+                      style={{ width: "100%" }}
+                      ref={tableGeneralRef}
+                      />
+                    </div>
+                    <div className="tab-pane fade" id="highlight-titles-psg" aria-labelledby="highlight-titles-psg-tab" role="tabpanel">
+                    <table
+                      id="datatable-highlight-title-psg"
+                      className="table table-striped table-bordered responsive-table datatable-visualization"
+                      style={{ width: "100%" }}
+                      ref={tablePSGRef}
+                      />
+                    </div>
+                    <div className="tab-pane fade" id="highlight-titles-commercial" aria-labelledby="highlight-titles-commercial-tab" role="tabpanel">
+                    <table
+                      id="datatable-highlight-title-commercial"
+                      className="table table-striped table-bordered responsive-table datatable-visualization"
+                      style={{ width: "100%" }}
+                      ref={tableCommercialRef}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
